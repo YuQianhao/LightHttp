9e7b075a82c648fcad7d551660368e24fd59b0f9
15265270444 <916640168@qq.com>
2019-10-30 12:57:52 +0800

    修复了时间戳转换失败问题。

--- LightHttp-Library/src/main/java/com/yuqianhao/lighthttp/Utils.java
+++ LightHttp-Library/src/main/java/com/yuqianhao/lighthttp/Utils.java
@@ -1,13 +1,30 @@
 package com.yuqianhao.lighthttp;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
 
+import java.lang.reflect.Type;
+import java.util.Date;
 import java.util.Map;
 
 public class Utils {
 
-    private static final Gson GSON=new Gson();
+    private static Gson GSON;
+
+    static {
+        GsonBuilder gsonBuilder=new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
+            public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                return new Date(json.getAsJsonPrimitive().getAsLong());
+            }
+        });
+        GSON=gsonBuilder.create();
+    }
 
     public static Gson getGson(){
         return GSON;
